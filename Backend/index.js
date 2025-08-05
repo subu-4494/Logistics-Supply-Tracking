de90f940b76ce8470c4b7d609d2b86cbd2249244
harshit <samshersinghbaghel152@gmail.com>
2025-08-06 04:10:00 +0530

    fornted deplyed link added in cors

--- Backend/index.js
+++ Backend/index.js
@@ -12,9 +12,9 @@ connectDB();
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin:  process.env.FRONTEND_URL,
   credentials: true, 
-})); 
+}));  
 
 app.use(express.json());
 app.use(cookieParser());
